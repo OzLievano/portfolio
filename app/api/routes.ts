88dfeece9664341dbb0f1b91d6a792bad63e9d2c
OzLievano <Osvaldoalievano@gmail.com>
2025-01-30 16:51:56 -0500

    fix: removed unused function

--- app/api/routes.ts
+++ app/api/routes.ts
@@ -1,31 +1,7 @@
-import { Skill, allSkillData, experiencePerLevel } from "@/utils/db";
+import { allSkillData, experiencePerLevel } from "@/utils/db";
 
 export const getSkills = () => {
   return allSkillData;
-};
-
-const updateSkillExperience = async (
-  skill: Skill,
-  hoursTrained: number
-): Promise<[Skill, ...Skill[]]> => {
-  try {
-    const skillData = await getSkills();
-
-    if (typeof hoursTrained !== "number" || hoursTrained < 0) {
-      throw new Error(
-        "Invalid hoursTrained value. It must be a non-negative number."
-      );
-    }
-
-    const totalExperienceGained = hoursTrained * 60000;
-
-    skill.currentExperience += totalExperienceGained;
-
-    return [skill, ...skillData];
-  } catch (error) {
-    console.error("Error updating skill experience:", error);
-    throw error;
-  }
 };
 
 export const getExperienceDelta = (
